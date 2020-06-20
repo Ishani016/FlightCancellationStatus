266b53708c4f635985b12924b6c119c01b339ef5
Ishani Parashar <ish.parashar@gmail.com>
2020-06-20 12:03:32 +0530

    Delete CancelService.java

--- src/main/java/com/example/service/CancelService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CancelService {
-	boolean cancelFlightForUser(Integer flightId, Integer userId) throws ClassNotFoundException;
-	String getCancelStatus(Integer flightId, Integer userId) throws ClassNotFoundException;
-}
